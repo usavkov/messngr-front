76a058ba2d39dd3e5c28558c05e02a8af8f34cc7
Yury Saukou <yury_saukou@epam.com>
2021-10-25 16:10:59 +0300

    add pages mocks

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,29 @@
+import { Switch, Route } from 'react-router-dom';
+
+import {
+  PAGE_SIGN_UP,
+  PAGE_LOGIN,
+} from '../constants';
+import {
+  SignupPage,
+  LoginPage,
+} from '../pages';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Init
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route
+      exact
+      path={PAGE_SIGN_UP}
+      component={SignupPage}
+    />
+    <Route
+      exact
+      path={PAGE_LOGIN}
+      component={LoginPage}
+    />
+  </Switch>
+);
 
 export default App;
---
+++ src/constants/routes.js
@@ -0,0 +1,2 @@
+export const PAGE_SIGN_UP = '/signup';
+export const PAGE_LOGIN = '/login';
