3d5fb7c7f6138d527ffce31996d82cb16cee00a7
Yury Saukou <yury_saukou@epam.com>
2021-10-25 15:47:14 +0300

    init app

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Init
+      </header>
+    </div>
+  );
+}
+
+export default App;
