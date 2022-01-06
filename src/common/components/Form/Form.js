fb176c4a764f847d568886ab48cd6a877d9cae64
Yury Saukou <yury_saukou@epam.com>
2022-01-06 10:58:13 +0300

    add base for Form smart component

---
+++ src/common/components/Form/Form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import { Box } from '@mui/material';
+
+// TODO: handle child inputs in deep level
+export const Form = ({
+  children,
+  defaultValues,
+  onSubmit,
+  ...props
+}) => {
+  const { register, handleSubmit } = useForm({ defaultValues })
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      {...props}
+    >
+      {React.Children.map(children, child => {
+        console.log(children, child);
+        return child.props.name
+          ? React.createElement(child.type, {
+              ...{
+                ...child.props,
+                key: child.props.name,
+                register,
+              }
+            })
+          : child;
+       })}
+    </Box>
+  )
+}
--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,6 +1,7 @@
 export { Avatar } from './Avatar';
 export { AvatarBadge } from './AvatarBadge';
 export { Checkbox } from './Checkbox';
+export { Form } from './Form';
 export { Message } from './Message';
 export { MessageBox } from './MessageBox';
 export { SearchInput } from './SearchInput';
